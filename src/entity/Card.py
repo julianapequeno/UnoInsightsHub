3072c6fe6e4cc849153e6156e4e1a0df82a09054
julianasantiago100 <julianafpsc@gmail.com>
2023-11-25 09:41:13 -0300

    fix: wildcard interference in game's flow, checking in user_choice attribute

--- src/entity/Card.py
+++ src/entity/Card.py
@@ -1,7 +1,8 @@
-class Card:
+class Card():
     def __init__(self, rank, color) -> None:
         self.rank = rank
         self.color = color
+        self.user_choice = None
 
     def __str__(self) -> str:
         return f"Card ({self.rank}, {self.color})"
