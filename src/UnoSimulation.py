47667a18b5a43fecabdefa684bcf669a7a0610e1
julianasantiago100 <julianafpsc@gmail.com>
2023-11-25 08:20:26 -0300

    refactor: adding prints

--- src/UnoSimulation.py
+++ src/UnoSimulation.py
@@ -71,8 +71,12 @@ class UnoSimulation:
     def round(self) -> SimulationOutputData:
         if self.STATUS_CAN_PLAY:
             self.initialize_players_with_cards()
-            self.bot.shuffle_cards()
+            self.print_simulation_scrip()
 
+            self.bot.shuffle_cards()
+            self.initialize_game_with_first_card()
+
+            self.print_game_beggining(self.CARD_ON_THE_TABLE)
             while True:
                 self.update_currently_player()
                 self.bot.check_if_deck_is_empty_and_refuel_deck()
@@ -95,6 +99,8 @@ class UnoSimulation:
 
                 if not player_passed_their_turn:
                     self.CARD_ON_THE_TABLE = card_thrown
+                    self.print_card_thrown(card_thrown)
+                    self.check_if_it_is_uno()
 
                     if self.player_has_won():
                         name = self.CURRENTLY_PLAYER.get_player_name()
@@ -102,19 +108,56 @@ class UnoSimulation:
 
                     card_thrown.execute_move(
                         self.bot, self.IA_PLAYERS_CIRCULAR_VECTOR)
+                else:
+                    self.print_player_passed_their_turn()
 
                 self.bot.INDEX_WHO_IS_PLAYING += 1
         else:
+            self.print_cant_run_UNO_error_message()
             return None
 
     def player_has_won(self):
-        return self.bot.winner(self.CURRENTLY_PLAYER.get_player_cards())
+        if self.bot.winner(self.CURRENTLY_PLAYER.get_player_cards()):
+            print(self.CURRENTLY_PLAYER.get_player_name()+' won the game')
+            return True
+        else:
+            return False
 
     def simulation_data(self, name):
         out = SimulationOutputData(
             name, self.INITIAL_PLAYERS_CARDS, self.first_card)
         self.reset_simulation()
         return out
+
+    def print_player_passed_their_turn(self):
+        print(self.CURRENTLY_PLAYER.get_player_name()+" has passed their turn")
+
+    def print_simulation_scrip(self):
+        print(">>>>>>>>> Launching UNO")
+        print(f"Simulating with {self.number_of_players} players")
+        print("* PLAYERS INITIAL CARDS: ")
+
+        print(self.IA_PLAYERS_CIRCULAR_VECTOR)
+        for i in range(0, self.number_of_players):
+            print(">>>> Player ", i)
+            for card in self.IA_PLAYERS_CIRCULAR_VECTOR.get_ia_player_by_index(i).get_player_cards():
+                print(f"Card [{card.rank},{card.color}]")
+
+    def print_game_beggining(self, card):
+        print(">> INITIAL CARD: ", str(card))
+        print(">>>>>>>>>>>>>>>>> GAME BEGINS")
+
+    def print_card_thrown(self, card):
+        print(self.CURRENTLY_PLAYER.get_player_name(),
+              ' has thrown ', self.CARD_ON_THE_TABLE)
+
+    def print_game_beggining(self, card):
+        print(">> INITIAL CARD: ", str(card))
+        print(">>>>>>>>>>>>>>>>> GAME BEGINS")
+
+    def check_if_it_is_uno(self):
+        if (self.bot.is_UNO(self.CURRENTLY_PLAYER.get_player_cards())):
+            print("UNO!! - "+self.CURRENTLY_PLAYER.get_player_name())
 
     def print_cant_run_UNO_error_message(self):
         print(
